d645391c1f18b9e42611ed91b8e75d25aa1d40b7
iwantbear <gyuhanhwang@gmail.com>
2025-04-10 10:25:19 +0900

    feat: two crops transform

--- datautils.py
+++ datautils.py
@@ -14,6 +14,17 @@ from torch.utils.data import Dataset
 import librosa  
 
 
+
+class TwoCropsTransform:
+    """Take two random crops of one image as the query and key."""
+    def __init__(self, base_transform):
+        self.base_transform = base_transform
+
+    def __call__(self, x):
+        q = self.base_transform(x)
+        k = self.base_transform(x)
+        return [q, k]
+    
 class AddWhiteNoise(nn.Module):
     def __init__(self, max_snr_db, min_snr_db):
         super(AddWhiteNoise, self).__init__()
