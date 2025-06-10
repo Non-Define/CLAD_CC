7a0d1f23c5f8345fc580b64b7368bd392526b76a
iwantvear <gyuhanhwang@gmail.com>
2025-06-10 15:13:51 +0900

    Nothing

--- contrastive_loss.py
+++ contrastive_loss.py
@@ -43,7 +43,6 @@ class InstanceLoss(nn.Module):
 
         return loss
 
-
 class ClusterLoss(nn.Module):
     def __init__(self, class_num, temperature, device):
         super(ClusterLoss, self).__init__()
